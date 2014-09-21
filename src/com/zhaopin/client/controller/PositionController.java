fbad4d7afbadb287577baaca3ec4db95394ff7a9
MapleWhisper <827605162@qq.com>
2014-09-21 19:02:25 +0800

    1.9.21.2

--- src/com/zhaopin/client/controller/PositionController.java
+++ src/com/zhaopin/client/controller/PositionController.java
@@ -4,6 +4,7 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.portlet.ModelAndView;
@@ -20,11 +21,12 @@ public class PositionController {
 	 * 显示职位详情页面
 	 * @return
 	 */
+	
 	@RequestMapping("/position/show/id/{id}")
-	public ModelAndView loginAdmin( @RequestParam int id,Model model){
+	public String loginAdmin( @PathVariable int id,Model model){
 		model.addAttribute("position",positionServer.getById(id));
-		
-		return new ModelAndView("client/position");
+		System.out.println("ok");
+		return "client/position";
 	}
 	
 	

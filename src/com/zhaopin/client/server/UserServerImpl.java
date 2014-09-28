e6d8a08341c33e4b3f3d08042fd0012302dc04dd
MapleWhisper <827605162@qq.com>
2014-09-28 21:58:11 +0800

    1.9.28.50

--- src/com/zhaopin/client/server/UserServerImpl.java
+++ src/com/zhaopin/client/server/UserServerImpl.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.zhaopin.client.server;
 
 
@@ -30,36 +30,3 @@ public class UserServerImpl extends BaseServerImpl<User> implements UserServer{
 		return u;
 	}
 }
-=======
-package com.zhaopin.client.server;
-
-
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.zhaopin.base.BaseServerImpl;
-import com.zhaopin.po.User;
-
-/**
- * 管理员登陆服务实现类
- * 
- * @author 于广路
- *
- */
-@Service("userServerImpl")
-@Transactional
-public class UserServerImpl extends BaseServerImpl<User> implements UserServer{
-	/**
-	 * 判断用户用户名密码是否正确
-	 * 
-	 * @return true:正确
-	 * 		   false:不正确	
-	 */
-	@Override
-	public User login(User user) {
-		User u = (User) getSessionFactory().openSession().createQuery("from User a where a.email = ? and a.password= ? ")
-			.setParameter(0, user.getEmail()).setParameter(1, user.getPassword()).uniqueResult();
-		return u;
-	}
-}
->>>>>>> branch 'master' of https://github.com/MapleWhisper/ZhaoPin.git
